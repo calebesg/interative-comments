68c7e33412b18623df3de52e3844f330dd5a499b
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-09 19:58:20 -0300

    render form

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-const loadData = async function () {
-  const response = await fetch("./data.json");
-  const data = await response.json();
+const parentElement = document.querySelector('.c-comment');
+
+const generateForm = function (user, type = 'NEW') {
+  return `
+    <form action="#" class="c-comment__form">
+      <picture>
+        <img
+          class="c-comment__form__avatar"
+          src="${user.image.png}"
+          loading="lazy"
+          alt="${user.username}"
+        />
+      </picture>
+
+      <textarea
+        class="c-comment__form__text"
+        name="comment"
+        id="comment"
+        cols="30"
+        rows="1"
+        placeholder="Add a comment..."
+        aria-label="Enter you comment"
+      ></textarea>
+
+      <button class="btn btn--blue">${
+        type === 'NEW' ? 'SEND' : 'REPLY'
+      }</button>
+    </form>
+  `;
 };
 
-// loadData();
+const loadData = async function () {
+  const response = await fetch('./data.json');
+  const data = await response.json();
+
+  if (!response.ok) throw new Error('Data not found!');
+
+  return data;
+};
+
+const app = async function (parentEl) {
+  try {
+    const { comments, currentUser } = await loadData();
+
+    const commentForm = generateForm(currentUser);
+    parentEl.insertAdjacentHTML('beforeend', commentForm);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+app(parentElement);
